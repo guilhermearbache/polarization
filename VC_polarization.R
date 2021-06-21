98ad7c8633e7ce06676ce720959310579bda09a2
guilhermearbache <guilhermearbache@gmail.com>
2021-06-20 23:37:04 -0300

    Final version before sending paper to EPSA. I calculated polarization of voters in the same dataset, instead of making other filtering only voters.

--- VC_polarization.R
+++ VC_polarization.R
@@ -1,5 +1,3 @@
-
-
 rm(list=ls()[!(ls() %in% c("cses_leg"))])
 #rm(list=ls())
 
@@ -35,38 +33,34 @@ cses_leg <- cses_leg %>% mutate (
   )
 )
 
+#Party voted polarization :
 
 cses_leg$extreme_voted <- abs(cses_leg$ideol_voted_LH_all - 5)
-
 cses_leg$extreme_expvoted <- abs(cses_leg$exp_ideol_voted_LH_all - 5)
+
+##### POLARIZATION OF VOTERS #####
+
+cses_leg$voter_extreme <- cses_leg$extreme
+cses_leg$voter_extreme[cses_leg$voted_LH == 0] <- NA
+
+#POLARIZATION OF NON-VOTERS
+
+cses_leg$abs_extreme <- cses_leg$extreme
+cses_leg$abs_extreme[cses_leg$voted_LH == 1] <- NA
 
 
 ##### AGRUPANDO #####
-grouped_leg <- cses_leg %>% select (country, election,cyear, ideol_self, exp_ideol_voted_LH_all,
-                                    ideol_voted_LH_all, starts_with("extreme"), compulsory,
-                                    compulsory_dummy, dalton_pol, ENEP,
-                                    gallagher, education, knowledge_adj, 
-                                    size_LH, freedom_house_1) %>% group_by (country, election, cyear) %>%
+grouped_leg <- cses_leg %>% select (country, election, contains("extreme"), 
+                                    dalton_pol, compulsory_dummy, 
+                                    compulsory, ENEP, gallagher,
+                                    cyear, ideol_self, 
+                                    exp_ideol_voted_LH_all,
+                                    ideol_voted_LH_all, education, knowledge_adj, 
+                                    size_LH, freedom_house_1) %>% 
+  group_by (country, election, cyear) %>%
   summarize_all (.funs = c(mean="mean"), na.rm = T)
 
 names(grouped_leg) <- gsub("_mean", "", names(grouped_leg))
-
-
-##### FILTERING ONLY VOTERS #####
-
-cses_voters <- cses_leg %>% filter (voted_LH == 1) #FILTRAR ELEITORES
-
-
-#MÉDIA - SUMMARIZE
-
-grouped_vot <- cses_voters %>% select (country, election, cyear, ideol_self, exp_ideol_voted_LH_all,
-                                       ideol_voted_LH_all, starts_with("extreme"), compulsory,
-                                       compulsory_dummy, dalton_pol, ENEP,
-                                       gallagher, education, knowledge_adj, 
-                                       size_LH, freedom_house_1) %>% group_by (country, election, cyear) %>%
-  summarize_all (.funs = c(mean="mean"), na.rm = T)
-
-names(grouped_vot) <- gsub("_mean", "", names(grouped_vot))
 
 
 ##### QOG DATA #####
@@ -86,67 +80,11 @@ grouped_leg$ethnic <- qog$al_ethnic2000[match(grouped_leg$cyear, qog$cyear)]
 grouped_leg$religion <- qog$al_religion2000[match(grouped_leg$cyear, qog$cyear)]
 grouped_leg$language <- qog$al_language2000[match(grouped_leg$cyear, qog$cyear)]
 
-grouped_vot$gini <- qog$wdi_gini[match(grouped_vot$cyear, qog$cyear)]
-grouped_vot$ethnic <- qog$al_ethnic2000[match(grouped_vot$cyear, qog$cyear)]
-grouped_vot$religion <- qog$al_religion2000[match(grouped_vot$cyear, qog$cyear)]
-grouped_vot$language <- qog$al_language2000[match(grouped_vot$cyear, qog$cyear)]
-
-
-##### GRÁFICOS #####
-
-ggplot(data=grouped_leg, aes(x=extreme, group=compulsory_dummy, color=compulsory_dummy)) +
-  geom_histogram() +
-  theme(
-    panel.spacing = unit(0.5, "lines"),
-    axis.ticks.x=element_blank() 
-  ) + labs (x="Distância ideológica mínima por eleitor")
-
-#VOTO: 
-
-ggplot(data=grouped_vot, aes(x=extreme_voted, group=compulsory_dummy, color=compulsory_dummy)) +
-  geom_density() +
-  theme(
-    panel.spacing = unit(0.5, "lines"),
-    axis.ticks.x=element_blank() 
-  ) + labs (x="Polarização - voto")
-
-ggplot(data=grouped_vot, aes(x=extreme_expvoted, group=compulsory_dummy, color=compulsory_dummy)) +
-  geom_density() +
-  theme(
-    panel.spacing = unit(0.5, "lines"),
-    axis.ticks.x=element_blank() 
-  ) + labs (x="Polarização - voto (experts)")
-
-#Maior polarização em países com compulsório, mais uniforme entre eles mas muitos casos que ficam próximos de 3, 
-#facultativo tem pico mais forte perto de 2
-
-
-#Polarização do voto e dos ELEITORES tendência parecida, mas de VOTO - EXPERTS não, aí fica muito próximo compulsório e facultativo.
-
-# ELEITORES (IDEOLOGIA PESSOAL) 
-
-ggplot(data=grouped_vot, aes(x=extreme, group=compulsory_dummy, color=compulsory_dummy)) +
-  geom_density() +
-  theme(
-    panel.spacing = unit(0.5, "lines"),
-    axis.ticks.x=element_blank() 
-  ) + labs (x="Polarização entre eleitores")
-
-# DALTON 
-
-ggplot(data=grouped_vot, aes(x=dalton_pol, group=compulsory_dummy, color=compulsory_dummy)) +
-  geom_density() +
-  theme(
-    panel.spacing = unit(0.5, "lines"),
-    axis.ticks.x=element_blank() 
-  ) + labs (x="Polarização entre eleitores")
-
 ##### BOXPLOTS #####
 
-grouped_vot$compulsory_dummy <- as.factor(grouped_vot$compulsory_dummy)
+#Little changes in data 
 grouped_leg$compulsory_dummy <- as.factor(grouped_leg$compulsory_dummy)
-
-grouped_leg$dalton_pol <- grouped_leg$dalton_pol/2
+grouped_leg$dalton_pol <- grouped_leg$dalton_pol/2 #Só para ficar igual os outros
 
 
 ggplot(grouped_leg, aes(x=extreme, y=compulsory_dummy,  color = compulsory_dummy)) + 
@@ -154,17 +92,17 @@ ggplot(grouped_leg, aes(x=extreme, y=compulsory_dummy,  color = compulsory_dummy
   labs (x="All citizen") +
   stat_summary(fun.y=mean, geom="point", size=2) 
 
-ggplot(grouped_vot, aes(x=extreme, y=compulsory_dummy,  color = compulsory_dummy)) + 
+ggplot(grouped_leg, aes(x=voter_extreme, y=compulsory_dummy,  color = compulsory_dummy)) + 
   geom_boxplot() + theme(legend.position="none" , axis.title.y = element_blank()) + 
-   labs (x="Voters") +
-  stat_summary(fun.y=mean, geom="point", size=2)
+  labs (x="Voters") +
+  stat_summary(fun.y=mean, geom="point", size=2) 
 
-ggplot(grouped_vot, aes(x=extreme_voted, y=compulsory_dummy,  color = compulsory_dummy)) + 
+ggplot(grouped_leg, aes(x=extreme_voted, y=compulsory_dummy,  color = compulsory_dummy)) + 
   geom_boxplot() + theme(legend.position="none", axis.title.y = element_blank() 
   ) + labs (x="Party voted (citizen score)") +
   stat_summary(fun.y=mean, geom="point", size=2)
 
-ggplot(grouped_vot, aes(x=extreme_expvoted, y=compulsory_dummy,  color = compulsory_dummy)) + 
+ggplot(grouped_leg, aes(x=extreme_expvoted, y=compulsory_dummy,  color = compulsory_dummy)) + 
   geom_boxplot() + theme(legend.position="none", axis.title.y = element_blank()  
   ) + labs (x="Party voted (expert score)") +
   stat_summary(fun.y=mean, geom="point", size=2)
@@ -219,23 +157,79 @@ stargazer(b1,m1,b2,m2) # Citizen e voter, o problema é que fica ruim de ver - d
 #1,2,3,4 por "citizen, etc." mas é melhor então a ordem !
 
 
-stargazer(b3,m3,b4,m4,b5)
+stargazer(b3,b4,b5)
+
+stargazer(m3,m4,m5)
 
 
-m1  <- glm(extreme ~ compulsory_dummy + gini, data = grouped_leg) 
+######  MODELOS SEM GINI #####
 
-ggplot(grouped_leg, aes(x = dalton_pol, y = extreme)) + 
-  geom_point(size = 2.5, color = "#0099f9") +
-  geom_text_repel(label = grouped_leg$election,  size=1.5) +  labs(
-    x = "Dalton",
-    y = "Citizen"
-  )
+g1 <- glm(extreme ~ compulsory_dummy + gallagher + ENEP +
+             ethnic, data = grouped_leg) 
 
+g2 <- glm(extreme ~ compulsory_dummy + gallagher + ENEP + 
+             ethnic, data = grouped_vot) 
+
+g3 <- glm(extreme_voted ~ compulsory_dummy + gallagher + ENEP + 
+             ethnic, data = grouped_vot) 
+
+g4 <- glm(extreme_expvoted ~ compulsory_dummy + gallagher + ENEP + 
+             ethnic, data = grouped_vot) 
+
+g5 <- glm(dalton_pol ~ compulsory_dummy + gallagher + ENEP + 
+             ethnic, data = grouped_leg) 
+
+stargazer(g1,g2,g3,g4,g5)
+
+##### PLOTS #####
+
+# color = "#0099f9"
+
+grouped_leg$compuls_cat <- "No"
+grouped_leg$compuls_cat[grouped_leg$compulsory_dummy == 1] <- "Yes"
+
+ggplot(grouped_leg, aes(x = dalton_pol, y = extreme, color = compuls_cat)) + 
+  geom_point(size = 1.6) +
+  labs( x = "Parliament (Dalton index)",
+    y = "Citizen",
+    color = "Compulsory vote"
+  ) + theme(legend.position="bottom", 
+            axis.title = element_text(size = 9),
+            legend.title = element_text(size = 8, face = "bold"),
+            legend.text = element_text(size = 8)) +
+  scale_color_manual(values = c("darkolivegreen4","deepskyblue3"))
+
+
+
+ggplot(grouped_leg, aes(x = voter_extreme, y = extreme, color = compuls_cat)) + 
+  geom_point(size = 1.6) +
+  labs( x = "Voters only",
+        y = "All citizen",
+        color = "Compulsory vote"
+  ) + theme(legend.position="bottom", 
+            axis.title = element_text(size = 9),
+            legend.title = element_text(size = 8, face = "bold"),
+            legend.text = element_text(size = 8)) +
+  scale_color_manual(values = c("darkolivegreen4","deepskyblue3"))
+
+
+
+ggplot(grouped_leg, aes(x = voter_extreme, y = abs_extreme, color = compuls_cat)) + 
+  geom_point(size = 1) + geom_smooth(method = "lm", se = FALSE)+ 
+  labs( x = "Voters",
+        y = "Absent",
+        color = "Compulsory vote"
+  ) + theme(legend.position="bottom", 
+            axis.title = element_text(size = 9),
+            legend.title = element_text(size = 8, face = "bold"),
+            legend.text = element_text(size = 8)) +
+  scale_color_manual(values = c("darkolivegreen4","blue"))
 
 
 ###### FUTURE IMPROVEMENTS #####
 
-#JUNTAR NO MESMO DATASET OS DADOS PARA ELEITORES APENAS - FAZER O FILTRO ANTES DE SUMMARIZE
-#TALVEZ NEM PRECISE FILTRAR, SÓ CRIAR UMA NOVA VERSÃO DE "EXTREME" COM NA PARA QUEM NÃO VOTOU
+#Balancear eleitores e não eleitores (só achar uma variável para turnout e multiplicar
+# turnout * extreme if voted ==1, 1-turnout * extreme se for não eleitor
+#aí tirar a média normal. 
 
 
